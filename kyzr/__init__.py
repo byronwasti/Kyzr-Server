acfbb52712a0af05a9a156140ed851b13a3605d4
byronwasti <byron.wasti@gmail.com>
2015-04-21 11:43:13 -0400

    figuring out what is borked

--- kyzr/__init__.py
+++ kyzr/__init__.py
@@ -32,12 +32,12 @@ def maps():
             else:
                 return "ID: " + id_request + " not found"
     if coords:
-        #center = [ float(sum(i)/len(i)) for i in coords ]
-        center = [10,10]
+        center = [ float(sum(i)/len(i)) for i in coords ]
+        #center = [10,10]
     #center = [42.2927482,   -71.2640407]
     #return render_template('maps.html', coords=json.dumps(coords), center=json.dumps(center))
-    #return render_template('error.html', coords=json.dumps(coords), center=json.dumps(center))
-    return render_template('error.html', coords=coords, center=center)
+    return render_template('error.html', coords=json.dumps(coords), center=json.dumps(center))
+    #return render_template('error.html', coords=coords, center=center)
 
 
 # Next two functions are for database 
