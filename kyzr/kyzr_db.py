c5db991330d80b3c3bd7a710fbf9bcdcd2343f63
byronwasti <byron.wasti@gmail.com>
2015-05-06 01:03:06 -0400

    SPACE MOTHERFUCKER. YOU NEEDS IT

--- kyzr/kyzr_db.py
+++ kyzr/kyzr_db.py
@@ -147,7 +147,7 @@ class dbEditor:
             {'_id':'comstat'},
             {'$set':{'#': new_num }},
             True)
-        USERS = self.find_user(pid1)['username'] +','+self.find_user(pid2)['username']
+        USERS = self.find_user(pid1)['username'] +', '+self.find_user(pid2)['username']
 
         cur_num = str(cur_num)
 
